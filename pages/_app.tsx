9948dcd5ebda2fc66035ac875cc636d1b709e613
Rutvij Sathe <rut26sathe@gmail.com>
2023-04-16 13:46:49 -0400

    Added Google Tag Manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2203570267369097"
+     crossOrigin="anonymous"></Script>
+<Script async src={`https://www.googletagmanager.com/gtag/js?id=G-FGWNPBN1HZ`}></Script>
+<Script id="google-analytics" strategy='afterInteractive'>
+  {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FGWNPBN1HZ);
+  `}
+</Script>
+  <Component {...pageProps} />
+  </>
 }
