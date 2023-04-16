d595c5aead1463a259a50cd7737b1351556056e1
Rutvij Sathe <rut26sathe@gmail.com>
2023-04-16 15:06:11 -0400

    gtm file created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,47 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 import Script from 'next/script'
+import { useEffect } from 'react'
+import * as gtag from '../lib/gtag'
+import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <>
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2203570267369097"
-     crossOrigin="anonymous"></Script>
-<Script async src={`https://www.googletagmanager.com/gtag/js?id=G-FGWNPBN1HZ`}></Script>
-<Script id="google-analytics" strategy='afterInteractive'>
-  {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-FGWNPBN1HZ);
-  `}
-</Script>
-  <Component {...pageProps} />
-  </>
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+        <Script
+        id="gtm-script"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default App
